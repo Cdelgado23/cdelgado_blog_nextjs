f0b10e1c757f82547fb426e7f09fae212c44ae08
cdelgado <cdelgadoguiberteau@gmail.com>
2021-11-02 18:59:27 +0100

    fix: info cards styling

--- src/components/FrontPageInfoCard.tsx
+++ src/components/FrontPageInfoCard.tsx
@@ -9,15 +9,14 @@ type Props = {
 };
 export default function InfoCard({ card }: Props) {
     return (
-        <DataCard color="#FFFF" header="Working on...">
-            <h3 style={{ margin: "0" }}>{card.card_title}</h3>
+        <DataCard color="#FFFF" header={card.card_title}>
+            <h3 style={{ margin: "0" }}>{card.card_subtitle}</h3>
             <div className={styles.anchor}>
                 <div className={styles.overlayedContent} style={{ display: "flex", flexDirection: "column", justifyContent: "start", alignItems: "center" }}>
                     <img src={card.card_image} className={styles.projectImg} />
                 </div>
                 <div className={styles.overlay}>
                     <div className={styles.overlayText}>
-                        <h2 >{card.card_subtitle}</h2>
                         <p>{card.card_summary}</p>
                     </div>
                 </div>
