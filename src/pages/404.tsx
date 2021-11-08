4c6642819c0b963640ee76f6924a041f7a0d6cce
cdelgado <cdelgadoguiberteau@gmail.com>
2021-11-08 19:39:35 +0100

    feat: add custom favicon
    Signed-off-by: cdelgado <cdelgadoguiberteau@gmail.com>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,6 +13,10 @@ export default function FourOhFour() {
             <div className={styles.container}>
                 <Head>
                     <title>Something went wrong 😢</title>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                    <link rel="manifest" href="/site.webmanifest"/>
                 </Head>
                 <Typography  variant="h3" component="h3">
                     Oops, I could not find what you are looking for 😢
